b2023881933ce0e6efe8141f48c72e3d84a6c86e
DiegoArte <94568408+DiegoArte@users.noreply.github.com>
2021-12-03 09:39:22 -0600

    Add files via upload

---
+++ Conversiones.py
@@ -0,0 +1,41 @@
+# -*- coding: cp1252 -*-
+def KmM():
+      Km=float(input("Dame los kilómetros"))
+      m=Km*1000
+      print "Eso es equivalente a",m,"metros"
+
+def MKm():
+      m=float(input("Dame los metros"))
+      Km=m/1000
+      print"Eso es equivalente a",Km,"kilómetros"
+      
+
+def MFt():
+      m=float(input("Dame los metros"))
+      ft=m*3.281
+      print "Eso es equivalente a",ft,"pies"
+
+def FtM():
+      Ft=float(input("Dame los pies"))
+      m=Ft/3.281
+      print"Eso es equivalente a",m,"metros"
+
+def cmP():
+      cm=float(input("Dame los centimetros"))
+      P=cm/2.54
+      print"Eso es queivalente a",P,"pulgadas"
+
+def ydM():
+      yd=float(input("Dame las yardas"))
+      m=yd/1.094
+      print
+
+def mileKm():
+      mile=float(input("Dame las millas"))
+      Km=mile*1.609
+
+def CF():
+      c=float(input("Dame grado Celsius"))
+      f=( c * (9/5)) + 32
+      print"Eso es equivalente a",f,"grado Fahrenheit"
+
